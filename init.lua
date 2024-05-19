e6d1e16a93f6ea26596c47d6b32afa1baaf271c1
Julian O'Grady <julianogrady07@gmail.com>
2024-05-19 00:03:33 -0400

    keybinds and lsp fix

--- init.lua
+++ init.lua
@@ -588,7 +588,7 @@ require('lazy').setup({
         },
 
         omnisharp = {
-          filetypes = { 'csharp' },
+          filetypes = { 'csharp', 'cs' },
         },
 
         prettier = {
@@ -746,7 +746,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-q>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
